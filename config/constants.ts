c4fc704d999afd87f407b9d71f440fe0f4c75d34
Ahsan Khan <57074386+ahsanjkhan@users.noreply.github.com>
2026-05-10 18:47:25 -0400

    Add metrics and alarms for session reminders (#6)

--- config/constants.ts
+++ config/constants.ts
@@ -31,3 +31,37 @@ export const CFN_OUTPUT_SESSION_REMINDERS_TABLE_DESCRIPTION = 'DynamoDB table na
 
 export const CFN_OUTPUT_SESSION_REMINDERS_LAMBDA_ID = 'SessionRemindersLambdaFunctionName';
 export const CFN_OUTPUT_SESSION_REMINDERS_LAMBDA_DESCRIPTION = 'Session reminders Lambda function name';
+
+// Alarm Notifier Lambda
+export const ALARM_NOTIFIER_LAMBDA_NAME = 'mathpracs-session-reminders-alarm-notifier';
+export const ALARM_NOTIFIER_LAMBDA_ID = 'AlarmNotifierFunction';
+export const ALARM_NOTIFIER_LAMBDA_RUNTIME = lambda.Runtime.PYTHON_3_10;
+export const ALARM_NOTIFIER_LAMBDA_ENTRY = '../MathPracsSessionRemindersLambda/alarm_notifier';
+export const ALARM_NOTIFIER_LAMBDA_INDEX = 'handler/lambda_function.py';
+export const ALARM_NOTIFIER_LAMBDA_HANDLER = 'lambda_handler';
+export const ALARM_NOTIFIER_LAMBDA_TIMEOUT = cdk.Duration.seconds(30);
+export const ALARM_NOTIFIER_LAMBDA_MEMORY_SIZE = 128;
+export const ALARM_NOTIFIER_LAMBDA_ENV_VAR_KEY_DISCORD_SECRETS_ARN = 'DISCORD_SECRETS_ARN';
+
+// SNS Topic
+export const ALARM_SNS_TOPIC_NAME = 'mathpracs-session-reminders-alarms';
+export const ALARM_SNS_TOPIC_ID = 'SessionRemindersAlarmTopic';
+
+// CloudWatch Alarms
+export const METRICS_NAMESPACE = 'MathPracs/SessionReminders';
+
+export const ALARM_STUDENT_INFO_DDB_ID = 'StudentInfoDDBAlarm';
+export const ALARM_STUDENT_INFO_DDB_NAME = 'mathpracs-session-reminders-student-info-ddb';
+export const ALARM_STUDENT_INFO_DDB_DESCRIPTION = 'Session Reminders: student data lookup failure';
+
+export const ALARM_TUTOR_INFO_DDB_ID = 'TutorInfoDDBAlarm';
+export const ALARM_TUTOR_INFO_DDB_NAME = 'mathpracs-session-reminders-tutor-info-ddb';
+export const ALARM_TUTOR_INFO_DDB_DESCRIPTION = 'Session Reminders: tutor data lookup failure';
+
+export const ALARM_API_FAILURE_ID = 'APIFailureAlarm';
+export const ALARM_API_FAILURE_NAME = 'mathpracs-session-reminders-api-failure';
+export const ALARM_API_FAILURE_DESCRIPTION = 'Session Reminders: external API call failure';
+
+export const ALARM_UNKNOWN_FAILURES_ID = 'UnknownFailuresAlarm';
+export const ALARM_UNKNOWN_FAILURES_NAME = 'mathpracs-session-reminders-unknown-failures';
+export const ALARM_UNKNOWN_FAILURES_DESCRIPTION = 'Session Reminders: unhandled exception in Lambda';
